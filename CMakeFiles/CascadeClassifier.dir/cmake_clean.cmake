7e5f2a1364b359519d95ece04e81ade3aeb20fb3
James Connor <alexj.27.97@gmail.com>
2018-01-05 15:29:22 +0100

    Initial Project

---
+++ CMakeFiles/CascadeClassifier.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CascadeClassifier.dir/CascadeClassifier.cpp.o"
+  "CascadeClassifier.pdb"
+  "CascadeClassifier"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CascadeClassifier.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
